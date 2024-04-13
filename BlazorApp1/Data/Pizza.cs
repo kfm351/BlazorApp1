2822d497b50cd348c00ddea9c7617eec4e1e8326
kfm351 <Elina.gav@yandex.ru>
2024-04-13 12:57:31 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp1/Data/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Pizza
+    {
+        public string Naming { get; set; }
+        public string Compound { get; set; }
+        public int Cost { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Pizza(string naming, string compound, int cost, string imageUrl)
+        {
+            Naming = naming;
+            Compound = compound;
+            Cost = cost;
+            ImageUrl = imageUrl;
+        }
+    }
+}
